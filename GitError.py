e379dcf428b218be0a412a5daa02efa08818f6a6
chenzhiting <zhitingchen19@outlook.com>
2025-05-15 18:16:35 +0800

    add: 添加更多的错误信息

--- GitError.py
+++ GitError.py
@@ -1,10 +1,12 @@
 class GitError(Exception):
     """
     Git 错误类，用于保存 Git 错误信息
+    包括使用的 git 客户端，git 指令，git 操作等
     """
-    def __init__(self,git_ops,git_args):
+    def __init__(self,client,git_ops,git_args):
+        self.client = client
         self.git_ops = git_ops
         self.git_args = git_args
 
     def __str__(self):
-        return f"错误时 git 操作: {self.git_ops}, git 参数: {self.git_args}"
+        return f"发生错误的 git 命令: {self.client} {self.git_ops} {self.git_args}"
